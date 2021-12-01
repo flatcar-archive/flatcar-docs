785c4be2485261f1919696623f3f726b081239c8
Kai Lueke <kailuke@microsoft.com>
2021-12-01 15:35:10 +0100

    update-to-flatcar.sh: only move old folder if it exists

--- update-to-flatcar.sh
+++ update-to-flatcar.sh
@@ -5,8 +5,10 @@ sudo rm -f /tmp/key
 curl -L -o /tmp/key https://raw.githubusercontent.com/flatcar-linux/coreos-overlay/flatcar-master/coreos-base/coreos-au-key/files/official-v2.pub.pem
 sudo umount /usr/share/update_engine/update-payload-key.pub.pem || true
 sudo mount --bind /tmp/key /usr/share/update_engine/update-payload-key.pub.pem
-sudo mv /etc/coreos /etc/flatcar
-sudo ln -s flatcar /etc/coreos
+if [ -d /etc/coreos ]; then
+  sudo mv /etc/coreos /etc/flatcar
+  sudo ln -s flatcar /etc/coreos
+fi
 sudo sed -i "/SERVER=.*/d" /etc/flatcar/update.conf
 echo "SERVER=https://public.update.flatcar-linux.net/v1/update/" | sudo tee -a /etc/flatcar/update.conf
 sudo rm -f /tmp/release
